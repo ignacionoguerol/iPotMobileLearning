f25bd75b50f0622cd128ee70e6c4a0a551c79a78
Unknown <ignacionoguerol@gmail.com>
2018-02-28 12:15:45 +0100

    update gitignore

---
+++ Web/fire/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDSLAQ-VDrQhweJ4Hclad8Y8s3PGQGLKqk',
+    authDomain: 'ipot-mobile-learning.firebaseapp.com',
+    databaseURL: 'https://ipot-mobile-learning.firebaseio.com',
+    projectId: 'ipot-mobile-learning',
+    storageBucket: 'ipot-mobile-learning.appspot.com',
+    messagingSenderId: '301944302387'
+  }
+};
